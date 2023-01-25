f8bc675179dd5d5ce16abc15664b83c1407c2cb5
Alex Badics <admin@stickman.hu>
2023-01-25 18:23:57 +0100

    project: add documentation

--- examples/read_sensors.rs
+++ examples/read_sensors.rs
@@ -2,13 +2,16 @@
 // This file is part of openreal
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 
-use openreal::Glasses;
+use openreal::{Glasses, GlassesEvent};
 
 fn main() {
     let glasses = Glasses::new().unwrap();
     println!("Got glasses, serial={}", glasses.serial().unwrap());
 
     loop {
-        println!("Event: {:?}", glasses.read_event().unwrap());
+        let event = glasses.read_event().unwrap();
+        if matches!(event, GlassesEvent::Magnetometer(_)) {
+            println!("Event: {:?}", event);
+        }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,95 @@
 // Copyright (C) 2023, Alex Badics
 // This file is part of openreal
 // Licensed under the MIT license. See LICENSE file in the project root for details.
+#![warn(missing_docs)]
+//! OpeNreal is a simplified driver for Nreal Air (and possibly other) AR glasses.
+//! It supports getting basic sensor data and setting up the display.
+//! It only uses [`rusb`] for communication.
+//!
+//! Example usage (in a thread, probably):
+//! ```ignore
+//! let glasses = Glasses::new().unwrap();
+//! loop {
+//!     match glasses.read_event().unwrap() {
+//!         GlassesEvent::Accelerometer(data) => ...,
+//!         GlassesEvent::Gyroscope(data) => ...,
+//!         GlassesEvent::Magnetometer(data) => ...,
+//!         GlassesEvent::Misc(data) => ...,
+//!     }
+//! }
+//! ```
+//!
+//! As opposed to Nreal's own API, this is all that you get, since this is what comes
+//! out of the hardware. To get quaternions, you should probably use a lib that implements
+//! Madgwicks algorithm or a proper EKF. One good choice is the `eskf` crate.
 
 use std::{io::Cursor, time::Duration};
 
 use byteorder::{ReadBytesExt, LE};
 use rusb::{request_type, Device, DeviceHandle, DeviceList, GlobalContext};
 
+/// The main structure representing a connected AR glasses
 pub struct Glasses {
     device_handle: DeviceHandle<GlobalContext>,
 }
 
+/// Possible errors resulting from `openreal` API calls
 #[derive(Debug, Clone)]
 pub enum Error {
+    /// An rusb error happened. See [`rusb::Error`] for specifics
     UsbError(rusb::Error),
+    /// No glasses were found.
     NotFound,
+    /// Other fatal error, usually a problem with the library itself, or
+    /// a device support issue. File a bug if you encounter this.
     Other(&'static str),
 }
 
 type Result<T> = std::result::Result<T, Error>;
 
+/// AR glasses sensor event, got from [`Glasses::read_event`]
 #[derive(Debug, Clone)]
 pub enum GlassesEvent {
+    /// Accelerometer data in m^2/s
     Accelerometer(SensorData3D),
+    /// Gyroscope data. Right handed rotation in rad/sec,
+    /// e.g. turning left is positive y axis.
     Gyroscope(SensorData3D),
+    /// Magnetometer data. Vector points to magnetic north (mostly)
     Magnetometer(SensorData3D),
+    /// Other sensor data
     Misc(MiscSensors),
 }
 
+/// Structure to hold a typical 3D sensor value (accelerometer, gyroscope, magnetomer)
 #[derive(Debug, Clone)]
 pub struct SensorData3D {
+    /// Timestamp, in device time, in microseconds
     pub timestamp: u64,
+    /// Sensor value in the X coordinate. Positive is Right.
     pub x: f32,
+    /// Sensor value in the Y coordinate. Positive is Up.
     pub y: f32,
+    /// Sensor value in the Z coordinate. Positive is Backwards.
     pub z: f32,
 }
 
+/// Container for other sensor data
 #[derive(Debug, Clone)]
 pub struct MiscSensors {
+    /// Keys pressed. On the Nreal Air, the only button is the brightness button,
+    /// And it corresponds to the value `4`
     pub keys: u8,
+    /// Proximity sensor report. `true` if the user is currently wearing the glasses
     pub proximity: bool,
 }
 
+/// Display mode used by [`Glasses::set_display_mode`]
 pub enum DisplayMode {
+    /// Picture should be same for both eyes (simple full HD mode)
     SameOnBoth = 0,
+    /// Set display to 3840*1080, where the left half is the left eye, the right half
+    /// is the right eye
     Stereo = 1,
 }
 
@@ -53,6 +99,8 @@ const INTERRUPT_IN_ENDPOINT: u8 = 0x82;
 const TIMEOUT: Duration = Duration::from_millis(250);
 
 impl Glasses {
+    /// Find a connected Nreal Air device and connect to it. (And claim the USB interface)
+    /// Only one instance can be alive at a time
     pub fn new() -> Result<Self> {
         let device = Self::get_rusb_device()?;
         let interface_num = Self::get_interrupt_interface(&device)?;
@@ -90,6 +138,7 @@ impl Glasses {
         ))
     }
 
+    /// Get the serial number of the glasses
     pub fn serial(&self) -> Result<String> {
         let mut result = [0u8; 0x40];
         self.device_handle.read_control(
@@ -110,6 +159,7 @@ impl Glasses {
         )
     }
 
+    /// Get a single sensor event. Blocks.
     pub fn read_event(&self) -> Result<GlassesEvent> {
         loop {
             let mut result = [0u8; 0x40];
@@ -141,6 +191,7 @@ impl Glasses {
         }
     }
 
+    /// Set the display mode of the glasses. See [`DisplayMode`]
     pub fn set_display_mode(&self, display_mode: DisplayMode) -> Result<()> {
         self.device_handle.write_control(
             request_type(
