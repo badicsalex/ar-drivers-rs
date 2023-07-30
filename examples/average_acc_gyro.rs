37628babf6073dd45017020229b328be6afa6222
Alex Badics <admin@stickman.hu>
2023-07-30 14:28:47 +0200

    examples: add average_acc_gyro example

---
+++ examples/average_acc_gyro.rs
@@ -0,0 +1,40 @@
+// Copyright (C) 2023, Alex Badics
+// This file is part of ar-drivers-rs
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+
+use std::time::{Duration, Instant};
+
+use ar_drivers::{any_glasses, GlassesEvent};
+use nalgebra::Vector3;
+
+fn main() {
+    let mut glasses = any_glasses().unwrap();
+    println!("Got glasses, serial={}", glasses.serial().unwrap());
+
+    let mut last_print = Instant::now();
+    let mut acc_sum = Vector3::zeros();
+    let mut gyro_sum = Vector3::zeros();
+    let mut n = 0;
+    loop {
+        if let GlassesEvent::AccGyro {
+            accelerometer,
+            gyroscope,
+        } = glasses.read_event().unwrap()
+        {
+            acc_sum += Vector3::new(accelerometer.x, accelerometer.y, accelerometer.z);
+            gyro_sum += Vector3::new(gyroscope.x, gyroscope.y, gyroscope.z);
+            n += 1;
+            if last_print.elapsed() > Duration::from_secs(1) {
+                println!(
+                    "Acceleration:\n{}Gyro:\n{}",
+                    acc_sum * (1.0 / n as f32),
+                    gyro_sum * (1.0 / n as f32)
+                );
+                last_print = Instant::now();
+                acc_sum = Default::default();
+                gyro_sum = Default::default();
+                n = 0;
+            }
+        }
+    }
+}
