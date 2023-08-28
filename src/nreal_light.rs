badicsalex/ar-drivers-rs

commit 973f0cffcd968f9f9d90997700d06b35fa071479

lib: rework display setting

src/nreal_light.rs
@@ -78,10 +78,11 @@ impl ARGlasses for NrealLight {
 
     fn set_display_mode(&mut self, display_mode: DisplayMode) -> Result<()> {
         let display_mode_byte = match display_mode {
-            DisplayMode::SameOnBoth => b'1',
-            DisplayMode::HalfSBS => b'2',
-            DisplayMode::Stereo => b'3',
-            DisplayMode::HighRefreshRate => b'4',
+            DisplayMode::SameOnBoth => 1,
+            DisplayMode::HalfSBS => 2,
+            DisplayMode::Stereo => 3,
+            DisplayMode::HighRefreshRateSBS => 4,
+            _ => return Err(Error::Other("Display mode not supported")),
         };
         let result = self.run_command(Packet {
             category: b'1',

examples/set_to_3d.rs
@@ -22,19 +22,22 @@ struct CliArgs {
 enum CliDisplayMode {
     /// Picture should be same for both eyes (simple full HD mode)
     #[value(name("2d"), alias("same-on-both"))]
-    SameOnBoth = 0,
+    SameOnBoth,
     /// Set display to 3840*1080 or 3840*1200,
     /// where the left half is the left eye, the right half is the right eye
     #[value(name("3d"), alias("sbs"), alias("stereo"))]
-    Stereo = 1,
+    Stereo,
     /// Set display to half-SBS mode, which presents itself as 1920*1080 resolution,
     /// but actually scales down everything to 960x540,then upscales to 3840x1080
     // "sbs2" matches how half-SBS referred to in stereo3d filter in mpv player
     #[value(name("halfsbs"), alias("sbs2"), alias("half-stereo"))]
-    HalfSBS = 2,
-    /// Set display to high refresh rate mode (120Hz in case of Rokid, 72Hz with Nreal)
-    #[value(name("120hz"), alias("high-refresh-rate"))]
-    HighRefreshRate = 3,
+    HalfSBS,
+    /// Set display to high refresh rate mode (120Hz in most cases)
+    #[value(name("high-refresh-rate-2d"), alias("120hz"))]
+    HighRefreshRate,
+    /// Set display to high refresh rate SBS (90Hz on the Nreal Air)
+    #[value(name("high-refresh-rate-3d"))]
+    HighRefreshRateSBS,
 }
 
 fn main() {
@@ -52,6 +55,7 @@ fn main() {
             CliDisplayMode::Stereo => DisplayMode::Stereo,
             CliDisplayMode::HalfSBS => DisplayMode::HalfSBS,
             CliDisplayMode::HighRefreshRate => DisplayMode::HighRefreshRate,
+            CliDisplayMode::HighRefreshRateSBS => DisplayMode::HighRefreshRateSBS,
         })
         .unwrap();
 

src/lib.rs
@@ -153,15 +153,17 @@ pub enum GlassesEvent {
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum DisplayMode {
     /// Picture should be same for both eyes (simple full HD mode)
-    SameOnBoth = 0,
+    SameOnBoth,
     /// Set display to 3840*1080 or 3840x1200,
     /// where the left half is the left eye, the right half is the right eye
-    Stereo = 1,
+    Stereo,
     /// Set display to half-SBS mode, which presents itself as 1920*1080 resolution,
     /// but actually scales down everything to 960x540,then upscales to 3840x1080
-    HalfSBS = 2,
-    /// Set display to high refresh rate mode (typically 120Hz)
-    HighRefreshRate = 3,
+    HalfSBS,
+    /// Set display to mirrored high refresh rate mode (typically 120Hz)
+    HighRefreshRate,
+    /// Set display to high refresh rate SBS mode
+    HighRefreshRateSBS,
 }
 
 /// Display side used by [`ARGlasses::view_matrix`]

src/mad_gaze.rs
@@ -82,7 +82,12 @@ impl ARGlasses for MadGazeGlow {
     }
 
     fn set_display_mode(&mut self, display_mode: DisplayMode) -> Result<()> {
-        let result = self.serial.do_command(b"S3D", &[display_mode as u8])?;
+        let display_mode = match display_mode {
+            DisplayMode::SameOnBoth => 0,
+            DisplayMode::Stereo => 1,
+            _ => return Err(Error::Other("Display mode not supported")),
+        };
+        let result = self.serial.do_command(b"S3D", &[display_mode])?;
         if result == [0] {
             Ok(())
         } else {

src/nreal_air.rs
@@ -77,6 +77,7 @@ impl ARGlasses for NrealAir {
             DisplayMode::HalfSBS => 8,
             DisplayMode::Stereo => 3,
             DisplayMode::HighRefreshRate => 11,
+            DisplayMode::HighRefreshRateSBS => 9,
         };
         let result = self.run_command(McuPacket {
             cmd_id: 0x08,

src/rokid.rs
@@ -138,6 +138,12 @@ impl ARGlasses for RokidAir {
     }
 
     fn set_display_mode(&mut self, display_mode: DisplayMode) -> Result<()> {
+        let display_mode = match display_mode {
+            DisplayMode::SameOnBoth => 0,
+            DisplayMode::Stereo => 1,
+            DisplayMode::HighRefreshRate => 3,
+            _ => return Err(Error::Other("Display mode not supported")),
+        };
         self.device_handle.write_control(
             request_type(
                 rusb::Direction::Out,
@@ -145,7 +151,7 @@ impl ARGlasses for RokidAir {
                 rusb::Recipient::Device,
             ),
             0x1,
-            display_mode as u16,
+            display_mode,
             0x1,
             &[0u8; 1],
             TIMEOUT,

commit 74515d1178e7edeb35eb0ed67197823b69bddcd4

nreal light: fix set_display_mode
Fixes regression introduced in 973f0cffcd968f9

Fixes #7

src/nreal_light.rs
@@ -78,10 +78,10 @@ impl ARGlasses for NrealLight {
 
     fn set_display_mode(&mut self, display_mode: DisplayMode) -> Result<()> {
         let display_mode_byte = match display_mode {
-            DisplayMode::SameOnBoth => 1,
-            DisplayMode::HalfSBS => 2,
-            DisplayMode::Stereo => 3,
-            DisplayMode::HighRefreshRateSBS => 4,
+            DisplayMode::SameOnBoth => b'1',
+            DisplayMode::HalfSBS => b'2',
+            DisplayMode::Stereo => b'3',
+            DisplayMode::HighRefreshRateSBS => b'4',
             _ => return Err(Error::Other("Display mode not supported")),
         };
         let result = self.run_command(Packet {